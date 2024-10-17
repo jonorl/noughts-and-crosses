3882853fe5996dc0f424a789b631b007d843e9c4
jonorl <39122230+jonorl@users.noreply.github.com>
2024-10-17 12:43:00 +0300

    Working version, checking for additional bugs

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-    noughtsAndCrosses.start();
-});
-
-const noughtsAndCrosses = (function() {
+(function() {
+    document.addEventListener("DOMContentLoaded", () => {
 
     // Player constructor
     function Player(name, marker) {
@@ -20,14 +17,11 @@ const noughtsAndCrosses = (function() {
     let turnMarker = true;
     let clickedBlock;
 
-
     function renderTurnMessage(){
         let playerOne = new Player(playerOneName.value, "X");
         let playerTwo = new Player(playerTwoName.value, "O");
-        if (playerOneName.value === "" || playerTwoName.value === ""){
-            turnMessage.style.color = "red";
-        }
-        else if (turnMarker === true){
+
+        if (turnMarker === true){
             turnMessage.style.color = "black";
             turnMessage.innerHTML = playerOne.name + " (X), please play now"
         }
@@ -60,100 +54,129 @@ const noughtsAndCrosses = (function() {
                 turnMarker = true;
             }
         }
-        else {
-        console.log("error");
-        turnMessage.innerHTML = "Box already selected";
-        return;
-        } 
     }
 
     function checkWinCondition(){
-        console.log(gameboard);
         if (gameboard[0] === "X" && gameboard[1] === "X" && gameboard[2] === "X") {
             turnMessage.innerHTML = playerOneName.value + " won!";
+            disableEventListeners();
             return;
         }
         else if (gameboard[3] === "X" && gameboard[4] === "X" && gameboard[5] === "X") {
             turnMessage.innerHTML = playerOneName.value + " won!";
+            disableEventListeners();
             return;
         }
         else if (gameboard[6] === "X" && gameboard[7] === "X" && gameboard[8] === "X") {
             turnMessage.innerHTML = playerOneName.value + " won!";
+            disableEventListeners();
             return;
         }
         else if (gameboard[0] === "X" && gameboard[3] === "X" && gameboard[6] === "X") {
             turnMessage.innerHTML = playerOneName.value + " won!";
+            disableEventListeners();
             return;
         }
         else if (gameboard[1] === "X" && gameboard[4] === "X" && gameboard[7] === "X") {
             turnMessage.innerHTML = playerOneName.value + " won!";
+            disableEventListeners();
             return;
         }
         else if (gameboard[2] === "X" && gameboard[5] === "X" && gameboard[8] === "X") {
             turnMessage.innerHTML = playerOneName.value + " won!";
+            disableEventListeners();
             return;
         }
         else if (gameboard[0] === "X" && gameboard[4] === "X" && gameboard[8] === "X") {
             turnMessage.innerHTML = playerOneName.value + " won!";
+            disableEventListeners();
             return;
         }
         else if (gameboard[2] === "X" && gameboard[4] === "X" && gameboard[6] === "X") {
             turnMessage.innerHTML = playerOneName.value + " won!";
+            disableEventListeners();
             return;
         }
         else if (gameboard[0] === "O" && gameboard[1] === "O" && gameboard[2] === "O") {
             turnMessage.innerHTML = playerTwoName.value + " won!";
+            disableEventListeners();
             return;
         }
         else if (gameboard[3] === "O" && gameboard[4] === "O" && gameboard[5] === "O") {
             turnMessage.innerHTML = playerTwoName.value + " won!";
+            disableEventListeners();
             return;
         }
         else if (gameboard[6] === "O" && gameboard[7] === "O" && gameboard[8] === "O") {
             turnMessage.innerHTML = playerTwoName.value + " won!";
+            disableEventListeners();
             return;
         }
         else if (gameboard[0] === "O" && gameboard[3] === "O" && gameboard[6] === "O") {
             turnMessage.innerHTML = playerTwoName.value + " won!";
+            disableEventListeners();
             return;
         }
         else if (gameboard[1] === "O" && gameboard[4] === "O" && gameboard[7] === "O") {
             turnMessage.innerHTML = playerTwoName.value + " won!";
+            disableEventListeners();
             return;
         }
         else if (gameboard[2] === "O" && gameboard[5] === "O" && gameboard[8] === "O") {
             turnMessage.innerHTML = playerTwoName.value + " won!";
+            disableEventListeners();
             return;
         }
         else if (gameboard[0] === "O" && gameboard[4] === "O" && gameboard[8] === "O") {
             turnMessage.innerHTML = playerTwoName.value + " won!";
+            disableEventListeners();
             return;
         }
         else if (gameboard[2] === "O" && gameboard[4] === "O" && gameboard[6] === "O") {
             turnMessage.innerHTML = playerTwoName.value + " won!";
+            disableEventListeners();
             return;
         }
         else if (gameboard.every(value => value === "X" || value === "O")){
             turnMessage.innerHTML = "It's a tie!";
+            disableEventListeners();
+            return;
         }
         else renderTurnMessage();
     }
 
-    blocks.forEach(block => {
-        block.addEventListener("click", function(e) {
-            clickedBlock = e.target.getAttribute('id');
+    function handleClick(e) {
+        clickedBlock = e.target.getAttribute("id");
+        if(playerOneName.value === "" || playerTwoName.value === ""){
+            turnMessage.style.color = "red";
+            turnMessage.innerHTML = "Please enter player names";
+        }
+        else if ((gameboard[clickedBlock] === "X" || gameboard[clickedBlock] === "O")){
+                turnMessage.innerHTML = "Box already selected";
+                return;
+            }
+
+        else {
             switchTurn();
-            if(playerOneName === "" && playerTwoName === ""){
+            if (playerOneName === "" && playerTwoName === "") {
                 renderTurnMessage();
+            } else if (turnMarker === true) {
+                e.target.innerHTML = "O";
+            } else if (turnMarker === false) {
+                e.target.innerHTML = "X";
             }
-            else if (turnMarker === true) {
-                block.innerHTML = "O";
-            } 
-            else if (turnMarker === false){
-                block.innerHTML = "X";
-            }
-        });
+        }
+    }
+    
+    blocks.forEach(block => {
+        block.addEventListener("click", handleClick);
     });
+
+    function disableEventListeners() {
+        blocks.forEach(block => {
+            block.removeEventListener("click", handleClick);
+        });
+    }
 
     btn.addEventListener("click", function() {
         window.location.reload();
@@ -163,8 +186,5 @@ const noughtsAndCrosses = (function() {
     playerOneName.addEventListener("keyup", renderTurnMessage);
     playerTwoName.addEventListener("keyup", renderTurnMessage);
 
-    // return {
-    //     start: switchTurn
-    // };
-
+});
 })();
